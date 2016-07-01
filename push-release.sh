23004ea153264c46a937a28b91ed09d6b2dd2b4b
Marcelo Rodriguez <cmarcelorp@gmail.com>
2016-07-01 17:08:39 -0300

    updating release

--- push-release.sh
+++ push-release.sh
@@ -4,6 +4,4 @@ npm run release
 wait
 git commit -am "updating release"
 git subtree push --prefix release origin gh-pages
-user@host:~$ read -n1 -r -p "Press any key to continue..." key
-[...]
-user@host:~$
+read -p "Press [Enter] key to start backup..."
