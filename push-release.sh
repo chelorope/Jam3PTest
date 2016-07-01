85276e98039ab01b3263536bf2b53c0e3eeae074
Marcelo Rodriguez <cmarcelorp@gmail.com>
2016-07-01 17:03:07 -0300

    updating release

--- push-release.sh
+++ push-release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-npm run release
+wait npm run release
 git commit -am "updating release"
 git subtree push --prefix release origin gh-pages
 user@host:~$ read -n1 -r -p "Press any key to continue..." key
